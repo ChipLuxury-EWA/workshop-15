f3f389c63489d190c2d1dd4bf620430958da4cef
Tom Portugez <chip_luxury@gmail.com>
2022-07-04 19:49:09 +0300

    service: sperated functions

--- workshops/seven-boom/src/services/counter.service.js
+++ workshops/seven-boom/src/services/counter.service.js
@@ -1,15 +1,27 @@
 export const checkSeven = (numba, numbaBoom) => {
-    console.log(`does ${numba} includes ${numbaBoom} ${includeNumba(numba, numbaBoom)}`)
-    console.log(numba % numbaBoom === 0)
-    if (
-        numba % numbaBoom === 0 || includeNumba(numba, numbaBoom)
-    ) {
-        return "BOOM"
+    const includeCheck = includeNumba(numba, numbaBoom);
+    const dividedCheck = dividedByNumba(numba, numbaBoom);
+
+    console.log(
+        `does ${numba} includes ${numbaBoom} ${includeCheck ? "yes" : "no"}`
+    );
+    console.log(
+        `does ${numba} Divided by ${numbaBoom} ${dividedCheck ? "yes" : "no"}`
+    );
+
+    if (includeCheck && dividedCheck) {
+        return "SUPA-BOOM";
+    } else if (includeCheck || dividedCheck) {
+        return "BOOM";
     } else {
-        return numba
+        return numba;
     }
 };
 
 const includeNumba = (numba, numbaBoom) => {
-    return numba.toString().indexOf(numbaBoom.toString()) !== -1
-}
+    return numba.toString().indexOf(numbaBoom.toString()) !== -1;
+};
+
+const dividedByNumba = (numba, numbaBoom) => {
+    return numba % numbaBoom === 0;
+};
