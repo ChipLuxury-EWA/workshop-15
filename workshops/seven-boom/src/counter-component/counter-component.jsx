2567b6b858eb01576553f4f4de4d43582291fd21
Tom Portugez <chip_luxury@gmail.com>
2022-07-04 19:27:22 +0300

    feat: 7 boom works

--- workshops/seven-boom/src/counter-component/counter-component.jsx
+++ workshops/seven-boom/src/counter-component/counter-component.jsx
@@ -1,15 +1,42 @@
 import "./counter-component.css";
-import {useState} from "react";
+import React, { useState } from "react";
 
-const CounterComponent = props => {
+const CounterComponent = (props) => {
     const [counterValue, setCounterValue] = useState(0);
+    const [counterValueBoom, setCounterValueBoom] = useState("");
+
+    React.useEffect(() => {checkSeven()}, [counterValue]);
+
+    const checkSeven = () => {
+        console.log(counterValue.toString().indexOf(7) !== -1)
+        console.log(counterValue % 7 === 0)
+        if (
+            counterValue % 7 === 0 ||
+            counterValue.toString().indexOf("7") !== -1
+        ) {
+            setCounterValueBoom("BOOM");
+        } else {
+            setCounterValueBoom(counterValue);
+        }
+    };
+
     return (
         <div className="counter-component">
-            <button className="counter-component__button" onClick={() => setCounterValue(counterValue + 1)}>+</button>
-            <div className="counter-component__value">{counterValue}</div>
-            <button className="counter-component__button" onClick={() => setCounterValue(counterValue - 1)}>-</button>
+            <button
+                className="counter-component__button"
+                onClick={() => setCounterValue(counterValue + 1)}
+            >
+                +
+            </button>
+            <div className="counter-component__value">{counterValueBoom}</div>
+            <button
+                className="counter-component__button"
+                onClick={() => setCounterValue(counterValue - 1)}
+            >
+                -
+            </button>
         </div>
-    )
-}
+    );
+};
 
 export default CounterComponent;
